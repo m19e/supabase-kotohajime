66c0844f70f89f47baeddff6038fbb7441bb6709
m19e <nexus.me7290@gmail.com>
2023-01-04 23:09:29 +0900

    Generate code for urql client

---
+++ codegen.ts
@@ -0,0 +1,16 @@
+import type { CodegenConfig } from "@graphql-codegen/cli"
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./src//consts"
+
+const config: CodegenConfig = {
+  schema: `${SUPABASE_URL}/graphql/v1?apikey=${SUPABASE_ANON_KEY}`,
+  documents: ["src/consts/**.ts"],
+  //   documents: ["**/*.tsx", "**/*.ts"],
+  generates: {
+    "./src/gql/index.tsx": {
+      plugins: ["typescript", "typescript-operations", "typescript-urql"],
+    },
+  },
+  ignoreNoDocuments: true,
+}
+
+export default config
