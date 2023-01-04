d39eaf2016467a53ad670b861da8d80e5f31a830
m19e <nexus.me7290@gmail.com>
2023-01-04 21:54:14 +0900

    Add consts for supabase inititalize

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,1 +1,6 @@
-export {}
+import { createClient } from "@supabase/supabase-js"
+
+export const SUPABASE_URL = process.env.NEXT_LOCAL_SUPABASE_URL!
+export const SUPABASE_ANON_KEY = process.env.NEXT_LOCAL_SUPABASE_ANON_KEY!
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
