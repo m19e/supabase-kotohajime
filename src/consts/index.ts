0d548e7ad430b61d40797e32bf3b46747d959e1f
m19e <nexus.me7290@gmail.com>
2023-01-04 22:07:39 +0900

    Add gql query consts

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -4,3 +4,37 @@ export const SUPABASE_URL = process.env.NEXT_LOCAL_SUPABASE_URL!
 export const SUPABASE_ANON_KEY = process.env.NEXT_LOCAL_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+export const ALL_TASKS = /* GraphQL */ `
+  query allTasks($orderBy: [tasksOrderBy!]) {
+    tasksCollection(orderBy: $orderBy) {
+      edges {
+        node {
+          title
+          is_completed
+          id
+        }
+      }
+    }
+  }
+`
+
+export const INSERT_TASKS = /* GraphQL */ `
+  mutation insertTasks($objects: [tasksInsertInput!]!) {
+    insertIntotasksCollection(objects: $objects) {
+      records {
+        title
+      }
+    }
+  }
+`
+
+export const UPDATE_TASKS = /* GraphQL */ `
+  mutation updateTasks($set: tasksUpdateInput!, $filter: tasksFilter) {
+    updatetasksCollection(set: $set, filter: $filter) {
+      records {
+        is_completed
+      }
+    }
+  }
+`
