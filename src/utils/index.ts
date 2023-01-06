90c9ebf9a9c826237cb4f1b90077fba1dbef7312
m19e <nexus.me7290@gmail.com>
2023-01-06 19:14:46 +0900

    Add util for urql exchange-auth

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,38 @@
-export {}
+import { makeOperation } from "@urql/core"
+import type { AuthConfig } from "@urql/exchange-auth"
+
+import { supabase, SUPABASE_ANON_KEY } from "@/consts"
+
+type AuthState = {
+  token: string
+}
+
+export const authConfig: AuthConfig<AuthState> = {
+  getAuth: async () => {
+    const session = (await supabase.auth.getSession()).data.session
+    const token = session ? session.access_token : SUPABASE_ANON_KEY
+    return { token }
+  },
+  addAuthToOperation: ({ authState, operation }) => {
+    if (!authState || !authState.token) {
+      return operation
+    }
+
+    const fetchOptions =
+      typeof operation.context.fetchOptions === "function"
+        ? operation.context.fetchOptions()
+        : operation.context.fetchOptions || {}
+
+    return makeOperation(operation.kind, operation, {
+      ...operation.context,
+      fetchOptions: {
+        ...fetchOptions,
+        headers: {
+          ...fetchOptions.headers,
+          Authorization: `Bearer ${authState.token}`,
+          apikey: SUPABASE_ANON_KEY,
+        },
+      },
+    })
+  },
+}
