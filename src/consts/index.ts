9f9a13ae2a967512fa05fef8cec30e13fd463579
m19e <nexus.me7290@gmail.com>
2023-01-07 19:24:33 +0900

    Fix env.loval vars name for next-public

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,8 +1,8 @@
-import { createClient } from "@supabase/supabase-js"
 import { gql } from "urql"
+import { createClient } from "@supabase/supabase-js"
 
-export const SUPABASE_URL = process.env.NEXT_LOCAL_SUPABASE_URL!
-export const SUPABASE_ANON_KEY = process.env.NEXT_LOCAL_SUPABASE_ANON_KEY!
+export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!
+export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
