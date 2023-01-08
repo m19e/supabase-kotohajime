08a98a27f2c6460daa39492bfd7e8648483ef86c
m19e <nexus.me7290@gmail.com>
2023-01-08 14:29:17 +0900

    Display, insert tasks

--- src/components/templates/Home.tsx
+++ src/components/templates/Home.tsx
@@ -3,6 +3,11 @@ import type { FormEventHandler } from "react"
 import type { Session } from "@supabase/supabase-js"
 
 import { supabase } from "@/consts"
+import {
+  AllTasksComponent,
+  InsertTasksComponent,
+  OrderByDirection,
+} from "@/gql"
 
 const useSession = () => {
   const [session, setSession] = useState<Session | null>(null)
@@ -64,7 +69,83 @@ const AppHeader = ({ isSignedIn }: { isSignedIn: boolean }) => {
 }
 
 const TodoList = () => {
-  return null
+  return (
+    <div className="flex flex-col h-full">
+      <AllTasksComponent
+        variables={{
+          orderBy: [
+            {
+              created_at: OrderByDirection.DescNullsFirst,
+            },
+          ],
+        }}
+      >
+        {({ data }) => {
+          const tasks = data!.tasksCollection!.edges
+          return (
+            <div className="overflow-y-auto grow min-h-0">
+              {tasks.map(({ node: task }) => (
+                <div key={task.id} className="flex p-1 text-lg">
+                  <div className="grow">{task.title}</div>
+                  <button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className={`w-6 h-6 ${
+                        task.is_completed
+                          ? "stroke-green-500"
+                          : "stroke-gray-500"
+                      }`}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              ))}
+            </div>
+          )
+        }}
+      </AllTasksComponent>
+      <InsertTasksComponent>
+        {({ executeMutation, fetching }) => {
+          const handleSubmit: FormEventHandler<HTMLFormElement> = async (
+            event
+          ) => {
+            const formElement = event.currentTarget
+            event.preventDefault()
+            const { title } = Object.fromEntries(
+              new FormData(event.currentTarget)
+            )
+            if (typeof title !== "string") return
+            if (!title) return
+            await executeMutation({ objects: [{ title }] })
+            formElement.reset()
+          }
+
+          return (
+            <form className="input-group" onSubmit={handleSubmit}>
+              <input
+                className="text-black input"
+                type="text"
+                name="title"
+                placeholder="New Task"
+              />
+              <button className="btn" type="submit" disabled={fetching}>
+                {fetching ? "Saving..." : "Save"}
+              </button>
+            </form>
+          )
+        }}
+      </InsertTasksComponent>
+    </div>
+  )
 }
 
 const LoginForm = () => {
